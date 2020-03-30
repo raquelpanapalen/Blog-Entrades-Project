0d2b526d9373c8a1d84d0d303267e8e61ecb3f02
raquelpanapalen <57674236+raquelpanapalen@users.noreply.github.com>
2020-03-30 17:47:04 +0200

    Updating ui package

--- blog/iu/InterficieUsuari.java
+++ blog/iu/InterficieUsuari.java
@@ -5,12 +5,40 @@
  */
 package edu.upc.etsetb.poo.blog.iu;
 
+import edu.upc.etsetb.poo.blog.controlador.Controlador;
+
 /**
  *
  * @author rachel
  */
-public class InterficieUsuari {
 
+/* Gestiona el menú principal i invoca al controlador per implementar cada una 
+ * de les opcions.
+ */
+public class InterficieUsuari {
+    
+    private Controlador controlador;
+    
+    //Constructor. Crea el controlador.
+    public InterficieUsuari(){
+        controlador = new Controlador();
+    }
+    
+    //Mostra les opcions del menú principal.
+    public void mostraOpcions(){
+        
+    }
+    
+    //Per cada valor d'op, executa el mètode corresponent del controlador.
+    public void executaOpcio(String op){
+        
+    }
+    
+    //Cicle principal d'execució: mostra opcions, llegeix opcio i executa opcio.
+    public void cicle(){
+        
+    }
+    
     /**
      * @param args the command line arguments
      */
