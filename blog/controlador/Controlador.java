d0a97323eea9b8c7366c145cc8d1c7f277209215
raquelpanapalen <57674236+raquelpanapalen@users.noreply.github.com>
2020-04-07 20:47:03 +0200

    Add files via upload

--- blog/controlador/Controlador.java
+++ blog/controlador/Controlador.java
@@ -5,7 +5,11 @@
  */
 package edu.upc.etsetb.poo.blog.controlador;
 
+import edu.upc.etsetb.poo.blog.domini.Entrada;
 import edu.upc.etsetb.poo.blog.domini.EntradesBlog;
+import edu.upc.etsetb.poo.blog.domini.Temps;
+import edu.upc.etsetb.poo.blog.serveis.IO;
+
 
 /**
  *
@@ -14,21 +18,26 @@ import edu.upc.etsetb.poo.blog.domini.EntradesBlog;
 public class Controlador {
     
     private EntradesBlog entrades_blog;
+    
     /* Constructor. Inicialitza les entrades del blog, crea i afegeix 
      * tres entrades per fer proves d'execució. */
     public Controlador(){
         entrades_blog = new EntradesBlog();
+        entrades_blog.afegeixOrdenat(new Entrada("t3", "x3", "2020-05-15 08:42:00"));
+        entrades_blog.afegeixOrdenat(new Entrada("t1", "x1", "2018-03-07 09:17:00"));
+        entrades_blog.afegeixOrdenat(new Entrada("t5", "x5", "2019-05-15"));
     }
+    
     //Mostra l'index de les entrades del blog.
     public void index(){
-
+        IO.mostraText(entrades_blog.index());
     }
     
     /* Mostra l'entrada amb iddentificador igual al valor del argument num.
      * Parameters: num - identificador de l'entrada
      */
     public void mostraEntrada(int num){
-        
+        IO.mostraText(entrades_blog.agafa(num).toString());
     }
     
     /* Demana el titol, el text, i el dia i hora a l'usuari, crea una entrada 
@@ -39,7 +48,14 @@ public class Controlador {
      * En el darrer cas s'assignarà el dia i hora actuals (ara).
      */
     public void novaEntrada(){
-        
+        IO.mostraText("Entrada nova:\n");
+        IO.mostraText("Títol? ");
+        String titol = IO.llegeixText();
+        IO.mostraText("Text? ");
+        String text = IO.llegeixText();
+        IO.mostraText("Dia i hora? (aaaa-mm-dd <hh:mm:ss>, Return=Ara) ");
+        String temps = IO.llegeixText();
+        entrades_blog.afegeixOrdenat(new Entrada(titol, text, temps));
     }
     
     /* Elimina l'entrada amb iddentificador igual al valor del argument num, 
@@ -47,25 +63,35 @@ public class Controlador {
      * Parameters: num - identificador de l'entrada a eliminar
      */
     public void eliminaEntrada(int num){
-        
+        IO.mostraText("Elimino: "+entrades_blog.elimina(num).toString());
     }
     
     /* Demana el criteri d'ordenació a l'usuari, l'assigna, i ordena segons el 
      * nou criteri. El criteri pot ser "num" o "titol".
      */
     public void ordena(){
-        
+        String criteri="";
+        do{
+            IO.mostraText("Criteri? (num/titol) ");
+            criteri = IO.llegeixText();
+        }while(!(criteri.equalsIgnoreCase("num") || criteri.equalsIgnoreCase("titol")));
+        entrades_blog.setCriteri(criteri);    
+        entrades_blog.ordena();
     }
     
     /* Demana a l'usuari els dies inicial i final i mostra totes les entrades 
      * del blog entre aquests dos dies (ambdós inclusius).
      */
     public void dates(){
-        
+        IO.mostraText("Dia inicial? (aaaa-mm-dd) ");
+        String inici = IO.llegeixText().trim();
+        IO.mostraText("Dia final? (aaaa-mm-dd) ");
+        String fi = IO.llegeixText().trim();
+        IO.mostraText(entrades_blog.entreDates(new Temps(inici), new Temps(fi)));
     }
     
     //Mostra totes les entrades del blog.
     public void mostraEntrades(){
-        
+        IO.mostraText(entrades_blog.toString());
     }
 }
