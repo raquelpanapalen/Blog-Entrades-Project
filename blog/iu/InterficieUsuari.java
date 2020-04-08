5341cd873a1beecf2661fc0e80cc8b2442cf897f
silviaferrer <62883007+silviaferrer@users.noreply.github.com>
2020-04-08 14:00:10 +0200

    Add files via upload

--- blog/iu/InterficieUsuari.java
+++ blog/iu/InterficieUsuari.java
@@ -6,10 +6,11 @@
 package edu.upc.etsetb.poo.blog.iu;
 
 import edu.upc.etsetb.poo.blog.controlador.Controlador;
+import edu.upc.etsetb.poo.blog.serveis.IO;
 
 /**
  *
- * @author rachel
+ * @author silvi
  */
 
 /* Gestiona el menú principal i invoca al controlador per implementar cada una 
@@ -26,17 +27,50 @@ public class InterficieUsuari {
     
     //Mostra les opcions del menú principal.
     public void mostraOpcions(){
-        
+        System.out.print("Opcions:\n\tindex\n\tmostra <num>\n\tnova\n\telimina <num>\n\tordena\n\tdates\n\ttotes\n\tfi\nIntrodueix una opció: ");
     }
     
     //Per cada valor d'op, executa el mètode corresponent del controlador.
     public void executaOpcio(String op){
-        
+        switch (op.charAt(0)) {
+            case 'i':
+                controlador.index();
+                break;
+            case 'm':
+                String[] parts=op.split(" ");
+                int num=Integer.parseInt(parts[1]);
+                controlador.mostraEntrada(num);
+                break;
+            case 'n':
+                controlador.novaEntrada();
+                break;
+            case 'e':
+                String[] partes=op.split(" ");
+                int n=Integer.parseInt(partes[1]);
+                controlador.eliminaEntrada(n);
+                break;
+            case 'o':
+                controlador.ordena();
+                break;
+            case 'd':
+                controlador.dates();
+                break;
+            case 't':
+                controlador.mostraEntrades();
+                break;
+            default:
+                break;
+        }
     }
     
     //Cicle principal d'execució: mostra opcions, llegeix opcio i executa opcio.
     public void cicle(){
-        
+        String opc="x";
+        while(opc.charAt(0)!='f'){
+            mostraOpcions();
+            opc=IO.llegeixText();
+            executaOpcio(opc);
+        }
     }
     
     /**
@@ -44,6 +78,10 @@ public class InterficieUsuari {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        InterficieUsuari iu=new InterficieUsuari();
+        System.out.println("Hola!");
+        iu.cicle();        
+        System.out.println("Adeu!");
     }
     
 }
