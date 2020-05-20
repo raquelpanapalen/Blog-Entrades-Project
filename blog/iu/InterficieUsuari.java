71b42d348ab6c17717b82a26b6930315d39e30e4
silviaferrer <62883007+silviaferrer@users.noreply.github.com>
2020-05-20 11:26:10 +0200

    Update InterficieUsuari.java

--- blog/iu/InterficieUsuari.java
+++ blog/iu/InterficieUsuari.java
@@ -12,80 +12,25 @@ import edu.upc.etsetb.poo.blog.serveis.IO;
 
 /**
  *
- * @author rachel
+ * @author silvi
  */
 
 /* Gestiona el menú principal i invoca al controlador per implementar cada una 
  * de les opcions.
  */
 public class InterficieUsuari {
-    
-    private Controlador controlador;
+    private Controlador contr;
+    private Menu menu;
     
     //Constructor. Crea el controlador.
     public InterficieUsuari(){
-        controlador = new Controlador();
+        this.contr= new Controlador();
+        this.menu= new MenuRegistrat(this.contr);
     }
-    
-    //Mostra les opcions del menú principal.
-    public void mostraOpcions(){
-        IO.mostraText("Opcions:\n" +
-            "   index\n" +
-            "   mostra num\n" +
-            "   nova\n" +
-            "   elimina num\n" +
-            "   ordena\n" +
-            "   dates\n" +
-            "   totes\n" +
-            "   fi\n");
-    }
-    
-    //Per cada valor d'op, executa el mètode corresponent del controlador.
-    public void executaOpcio(String op){
-        if (!(op.equals(""))) {
-            String[] parts = op.split(" ");
-            if (op.charAt(0)=='i' && "index".contains(op))
-                controlador.index();
-            
-            else if (op.charAt(0)=='n' && "nova".contains(op))
-                controlador.novaEntrada();
-            
-            else if (op.charAt(0)=='o' && "ordena".contains(op))
-                controlador.ordena();
-            
-            else if (op.charAt(0)=='d' && "dates".contains(op))
-                controlador.dates();
-            
-            else if (op.charAt(0)=='t' && "totes".contains(op))
-                controlador.mostraEntrades();
-            
-            else if (op.charAt(0)=='m' && "mostra".contains(parts[0])
-                    && parts.length==2)
-                controlador.mostraEntrada(Integer.parseInt(parts[1]));
-            
-            else if (op.charAt(0)=='e' && "elimina".contains(parts[0])
-                    && parts.length==2)
-                controlador.eliminaEntrada(Integer.parseInt(parts[1]));
-            
-            else if (op.charAt(0)=='f' && "fi".contains(op))
-                IO.mostraText("Adeu!\n");
-            
-            else
-                IO.mostraText("Opció incorrecta\n");
-        }
-        else
-            IO.mostraText("Opció incorrecta\n");
-    }
-    
+       
     //Cicle principal d'execució: mostra opcions, llegeix opcio i executa opcio.
     public void cicle(){
-        String op="";
-        while (!(op.equalsIgnoreCase("f") || op.equalsIgnoreCase("fi"))){
-            mostraOpcions();
-            IO.mostraText("Introdueix una opció: ");
-            op = IO.llegeixText().toLowerCase().trim();
-            executaOpcio(op);
-        }
+        menu.cicle();
     }
     
     /**
